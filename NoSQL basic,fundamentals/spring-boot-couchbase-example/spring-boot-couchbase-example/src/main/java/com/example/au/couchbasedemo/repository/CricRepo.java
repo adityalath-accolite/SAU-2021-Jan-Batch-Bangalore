035cf7754fb540ceb818f07e19d52c21b55c7935
adityalath-accolite <77058788+adityalath-accolite@users.noreply.github.com>
2021-01-13 21:58:29 +0530

    Delete NoSQL basic,fundamentals directory

--- NoSQL basic,fundamentals/spring-boot-couchbase-example/spring-boot-couchbase-example/src/main/java/com/example/au/couchbasedemo/repository/CricRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.au.couchbasedemo.repository;
-import org.springframework.data.repository.CrudRepository;
-
-import com.example.au.couchbasedemo.model.Cricket;
-
-public interface CricRepo extends CrudRepository<Cricket, String> {
-	
-}
